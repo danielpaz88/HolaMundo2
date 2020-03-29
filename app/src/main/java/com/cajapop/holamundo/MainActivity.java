a765f55b36429249bb8efcb3c4041ff62db40586
danielpaz88 <53090451+danielpaz88@users.noreply.github.com>
2020-03-29 16:30:54 -0300

    Initial commit

---
+++ app/src/main/java/com/cajapop/holamundo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.cajapop.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public static void main (String[]args){
+        //System.out.println("Hola gente");
+        //lienzo.setBackgroundResource(R.drawable.marcianito);
+    }
+
+}
